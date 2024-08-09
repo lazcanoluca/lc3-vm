2ec7a8e3e0b70b259e0b59b4daa6ca482f06b142
Luca Lazcano <lazcanoluca@gmail.com>
2024-08-09 11:40:13 -0300

    Implement `LEA` instruction

---
+++ src/instructions/lea.rs
@@ -0,0 +1,57 @@
+use crate::{
+    registers::{Register, Registers},
+    utils::sign_extend,
+};
+
+pub struct Lea {
+    dr: Register,
+    pc_offset9: u16,
+}
+
+impl Lea {
+    pub fn from_bits(bits: u16) -> Self {
+        let dr = Register::try_from((bits >> 9) & 0b111).unwrap();
+        let pc_offset9 = sign_extend(bits & 0x1ff, 9);
+
+        Self { dr, pc_offset9 }
+    }
+
+    pub fn execute(&self, registers: &mut Registers) {
+        registers.set(self.dr, registers.get(Register::PC) + self.pc_offset9);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{instructions, registers};
+
+    use super::*;
+
+    #[test]
+    fn test_bits_to_lea() {
+        // LEA  R3, 0x7
+        let bits = 0b1110_011_000000111;
+
+        let instruction = Lea::from_bits(bits);
+
+        assert_eq!(instruction.dr, Register::R3);
+        assert_eq!(instruction.pc_offset9, 7);
+    }
+
+    #[test]
+    fn test_lea_works() {
+        let mut registers = Registers::default();
+        registers.set(Register::PC, 0x1000);
+
+        let instruction = Lea {
+            dr: Register::R3,
+            pc_offset9: 0x7000,
+        };
+
+        assert_eq!(registers.get(Register::R3), 0);
+
+        instruction.execute(&mut registers);
+
+        assert_eq!(registers.get(Register::R3), 0x8000);
+    }
+}

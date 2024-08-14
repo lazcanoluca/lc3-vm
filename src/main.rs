9b657607e2da85610a1fd69452158c6cc7d24abb
Luca Lazcano <lazcanoluca@gmail.com>
2024-08-14 12:28:50 -0300

    Parse arguments with `clap`

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use std::{env, fs::File, io::BufReader, process::exit};
+use std::{fs::File, io::BufReader, path::PathBuf};
 
 use byteorder::{BigEndian, ReadBytesExt};
+use clap::{value_parser, Arg, Command};
 use memory::Memory;
 use registers::Registers;
 
@@ -16,38 +17,42 @@ mod traps;
 mod utils;
 mod vm;
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
+fn load_memory_from_file(memory: &mut Memory, file: File) {
+    let mut file = BufReader::new(file);
 
-    if args.len() < 2 {
-        println!("Usage: cargo run [image-file1] ...\n");
-        exit(2);
+    let mut addr = file
+        .read_u16::<BigEndian>()
+        .expect("error reading base address");
+
+    while let Ok(bits) = file.read_u16::<BigEndian>() {
+        memory.write(addr, bits);
+        addr = addr.checked_add(1).expect("file too large");
     }
+}
+
+fn main() {
+    let matches = Command::new("cargo run")
+        .about("An LC-3 virtual machine")
+        .arg(
+            Arg::new("image")
+                .short('i')
+                .long("img")
+                .value_parser(value_parser!(PathBuf))
+                .required(true)
+                .help("The path to the object file"),
+        )
+        .get_matches();
+
+    let obj_file = matches.get_one::<PathBuf>("image").unwrap();
+    let f = File::open(obj_file).unwrap();
 
     let mut memory = Memory::default();
-
-    for arg in args[1..].iter() {
-        let f = File::open(arg).unwrap_or_else(|_| panic!("couldn't open: {}", arg));
-        let mut f = BufReader::new(f);
-
-        let base_addr = f
-            .read_u16::<BigEndian>()
-            .expect("error reading base address");
-
-        let mut addr = base_addr;
-
-        while let Ok(bits) = f.read_u16::<BigEndian>() {
-            memory.write(addr, bits);
-            addr += 1;
-        }
-    }
+    load_memory_from_file(&mut memory, f);
 
     let registers = Registers::default();
-
     let mut vm = Vm::new(registers, memory);
 
     let termios = Termios::from_fd(STDIN).unwrap();
-
     setup_terminal(termios);
 
     vm.instruction_cycle();
